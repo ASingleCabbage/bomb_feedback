689d15362ab5947f74b1f7d6bd60a974229183c7
Singular Cabbage <daniellangamma@gmail.com>
2018-11-03 20:49:26 -0400

    Initial commit

---
+++ boom.py
@@ -0,0 +1,43 @@
+import requests
+import login_creds
+from requests.auth import HTTPBasicAuth
+from bs4 import BeautifulSoup
+import time
+import simpleaudio as sa
+auth = HTTPBasicAuth(login_creds.username, login_creds.password)
+
+s = requests.Session()
+sleepTime = 2
+
+def getStats():
+    r = s.post('https://www.cs.tufts.edu/comp/40/bombstats.html', auth=auth)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    explodeSum = 0
+    defuseSum = 0
+
+    for tr in soup.find_all('tr')[2:]:
+        tds = tr.find_all('td')
+
+        explodeSum += int((tds[2].contents)[0])
+        defuseSum += int((tds[1].contents)[0])
+    print('total defused: {} total exploded: {}'.format(defuseSum, explodeSum))
+    return (explodeSum, defuseSum)
+
+sums = getStats()
+cachedExplodeSum = sums[0]
+cachedDefuseSum = sums[1]
+
+while True:
+    if sums[0] != cachedExplodeSum and sums[0] != 0:
+        print("Bomb exploded")
+        wave_obj = sa.WaveObject.from_wave_file('explode.wav')
+        play_obj = wave_obj.play()
+        cachedExplodeSum = sums[0]
+    if sums[1] != cachedDefuseSum and sums[1] != 0:
+        wave_obj = sa.WaveObject.from_wave_file('defused.wav')
+        play_obj = wave_obj.play()
+        cachedDefuseSum = sums[1]
+        print("Phase defused")
+    time.sleep(2)
+    sums = getStats()
